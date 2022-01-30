3b71560bda437249cb7d6201b572be521564ceb0
Alessandro Corradi <corradi.a@maxmara.it>
2022-01-30 10:52:46 +0100

    feat: update compile script

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-coffee -in main.bn -out run
-rm run.s
+case $1 in
+    clean) rm -f *.ll *.s run a.out a.out.ll a.out.s ;;
+    install) echo "TODO!" ;;
+
+    *)
+        coffee -in main.bn -out run
+        ;;
+esac
