52253e747f633a22ab8204c9167ae4d5244d63f3
Alessandro Corradi <alecrd98@gmail.com>
2022-01-31 22:15:48 +0100

    feat: add test template

--- src/build.sh
+++ src/build.sh
@@ -5,6 +5,10 @@ case $1 in
         rm -f *.ll *.s run a.out a.out.ll a.out.s
         ;;
 
+    test)
+        find . -name '*.test.bn' -exec sh -c "coffee -in {} && ./a.out " \;
+        rm -rf a.out
+        ;;
     install)
         # 1. compile the project
         $0 build
