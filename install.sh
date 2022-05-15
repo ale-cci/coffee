e7d0970a3fcb60de7e46cd876fcd4815f75f9db0
ale-cci <alecrd98@gmail.com>
2022-05-15 09:56:49 +0200

    feat: add install.sh script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Script for system-wide coffee installation
+set -e
+
+COFFEE_ROOT=${COFFEE_ROOT:-$HOME/.coffee}
+SRC_DIR=./src/
+
+# Create installation directory structure
+mkdir -p "$COFFEE_ROOT/bin" "$COFFEE_ROOT/lib"
+
+llc "$SRC_DIR/coffee.ll" -o coffee.s
+gcc coffee.s -o coffee
+rm coffee.s
+
+cp "$SRC_DIR/syntax.txt" "$COFFEE_ROOT/bin"
+mv coffee "$COFFEE_ROOT/bin"
+
+echo " ☕ coffee is ready!"
+
+warn() {
+ echo ""
+ echo ""
+ echo "WARNING: seems like '$COFFEE_ROOT/bin' is not in \$PATH. Add the following to your shell init file:"
+ echo ""
+ echo " export PATH=\"\$PATH:$COFFEE_ROOT/bin\""
+ echo ""
+ echo "Thanks <3"
+ echo ""
+}
+
+[[ -z "$(which coffee)" ]] && warn
